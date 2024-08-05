13b4164d3be2b5abc5f387e6f6527950b9f074e2
Swayam Shalgar <swayamshalgar1211@gmail.com>
2024-08-05 22:19:13 +0530

    jwt and bcrypt

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import mongooseAggregatePaginate from'mongoose-aggregate-paginate-v2';
 const videoSchema = new Schema({
     videoFile: {
         type: String,
@@ -35,4 +35,5 @@ const videoSchema = new Schema({
     }
 },{timestamps:true});
 
+videoSchema.plugin(mongooseAggregatePaginate)
 export const Video = mongoose.model("Video",videoSchema);
